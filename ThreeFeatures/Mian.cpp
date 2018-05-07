2b240e8f25e5a627e2d3653da0585da935d2c03c
weienjun <2519812358@qq.com>
2018-05-07 20:58:39 +0800

    类特性

---
+++ ThreeFeatures/Mian.cpp
@@ -0,0 +1,16 @@
+#include<stdlib.h>
+#include"Package.h"
+#include"Inherit.h"
+#include"Polymorphic.h"
+
+int main()
+{
+	//test();
+//	test2();
+//	test3();
+//	test4();
+	test5();
+  	system("pause");
+
+	return 0;
+}
